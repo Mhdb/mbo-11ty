5436f97dbe1169b92abd025d1020d3c34f1051d3
med <socialgitkraken@zitk.fr>
2020-06-17 14:53:56 +0200

    Clean

--- src/_data/navigation.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  header: [
-    { text: 'Homepage', link: '/' },
-    { text: 'About', link: '/about/' },
-    { text: '404', link: '/404.html' },
-    { text: 'Landing', link: '/landing/' },
-
-  ],
-}
--- src/_scripts/_main.js
+++
@@ -1,7 +0,0 @@
-import '../_styles/_main.css'
-import './svelte'
-
-// DEV_MODE gets replaced at build time based on ENV!
-if (DEV_MODE) {
-  console.log('Dev mode is currently enabled.')
-}
