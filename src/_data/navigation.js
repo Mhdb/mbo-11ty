a0906e430c262a88bb2f12c26e0cdcbc2ff571e8
med <socialgitkraken@zitk.fr>
2020-06-17 14:20:47 +0200

    New

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -3,5 +3,7 @@ module.exports = {
     { text: 'Homepage', link: '/' },
     { text: 'About', link: '/about/' },
     { text: '404', link: '/404.html' },
+    { text: 'Landing', link: '/landing/' },
+
   ],
 }
